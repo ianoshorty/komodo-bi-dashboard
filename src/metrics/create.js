56afc8983962c97110349f7f8b4e25b70649c6ed
Ian Outterside <ian@komododigital.co.uk>
2019-10-11 16:12:07 +0100

    Update metric values, add postman collection for testing

--- src/metrics/create.js
+++ src/metrics/create.js
@@ -1,51 +1,21 @@
-const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+const AWS = require("aws-sdk"); // eslint-disable-line import/no-extraneous-dependencies
 const uuid = require("uuid"); // eslint-disable-line import/no-extraneous-dependencies
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-module.exports.list = (event, context, callback) => {
-
-  const params = {
-    TableName: process.env.DYNAMODB_TABLE
-  };
-
-  // fetch all metrics from the database
-  dynamoDb.scan(params, (error, result) => {
-    // handle potential errors
-    if (error) {
-      console.error(error);
-      callback(null, {
-        statusCode: error.statusCode || 501,
-        headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t fetch the metrics.',
-      });
-      return;
-    }
-
-    // create a response
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(result.Items),
-    };
-    callback(null, response);
-  });
-
-};
-
 module.exports.create = (event, context, callback) => {
-  
   const data = JSON.parse(event.body);
   const timestamp = new Date().getTime();
 
   if (typeof data.label !== "string") {
     console.error("Validation Failed");
-    
+
     callback(null, {
       statusCode: 400,
       headers: { "Content-Type": "text/plain" },
       body: "Couldn't create the metric item."
     });
-    
+
     return;
   }
 
@@ -54,7 +24,7 @@ module.exports.create = (event, context, callback) => {
     Item: {
       id: uuid.v1(),
       label: data.label,
-      count: 0,
+      metricCount: 0,
       createdAt: timestamp,
       updatedAt: timestamp
     }
---
+++ src/metrics/set.js
@@ -0,0 +1,54 @@
+const AWS = require("aws-sdk"); // eslint-disable-line import/no-extraneous-dependencies
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.set = (event, context, callback) => {
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+
+  // validation
+  if (typeof data.metricCount !== "number") {
+    console.error("Validation Failed");
+    callback(null, {
+      statusCode: 400,
+      headers: { "Content-Type": "text/plain" },
+      body: "Couldn't set the count for the metric."
+    });
+    return;
+  }
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Key: {
+      id: event.pathParameters.id
+    },
+    ExpressionAttributeValues: {
+      ":metricCount": data.metricCount,
+      ":updatedAt": timestamp
+    },
+    UpdateExpression:
+      "SET metricCount = :metricCount, updatedAt = :updatedAt",
+    ReturnValues: "ALL_NEW"
+  };
+
+  // update the todo in the database
+  dynamoDb.update(params, (error, result) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { "Content-Type": "text/plain" },
+        body: "Couldn't fetch the metric item."
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result.Attributes)
+    };
+    callback(null, response);
+  });
+};
