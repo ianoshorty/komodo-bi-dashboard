3d1803422ab1af3214ad20883386bd6cc8a32829
Ian Outterside <ian@komododigital.co.uk>
2019-10-11 15:49:23 +0100

    Add a metric

--- src/metrics.js
+++ src/metrics.js
@@ -1,11 +1,13 @@
 const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies
+const uuid = require("uuid"); // eslint-disable-line import/no-extraneous-dependencies
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
-const params = {
-  TableName: process.env.DYNAMODB_TABLE,
-};
 
 module.exports.list = (event, context, callback) => {
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE
+  };
 
   // fetch all metrics from the database
   dynamoDb.scan(params, (error, result) => {
@@ -28,4 +30,54 @@ module.exports.list = (event, context, callback) => {
     callback(null, response);
   });
 
+};
+
+module.exports.create = (event, context, callback) => {
+  
+  const data = JSON.parse(event.body);
+  const timestamp = new Date().getTime();
+
+  if (typeof data.label !== "string") {
+    console.error("Validation Failed");
+    
+    callback(null, {
+      statusCode: 400,
+      headers: { "Content-Type": "text/plain" },
+      body: "Couldn't create the metric item."
+    });
+    
+    return;
+  }
+
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      id: uuid.v1(),
+      label: data.label,
+      count: 0,
+      createdAt: timestamp,
+      updatedAt: timestamp
+    }
+  };
+
+  // write the todo to the database
+  dynamoDb.put(params, error => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { "Content-Type": "text/plain" },
+        body: "Couldn't create the metric item."
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(params.Item)
+    };
+    callback(null, response);
+  });
 };
